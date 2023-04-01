76ab318aa86508d8c37d1371b8b1c0fe6a837358
MARCOS <saurabh887799@gmail.com>
2023-04-02 02:34:35 +0530

    Added Home section, Header and Footer

---
+++ myapp/src/App.jsx
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom" 
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer.jsx";
+
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss"
+
+function App(){
+    return(
+        <>
+         <Router>
+             <Header/>
+            <Routes>
+               <Route path="/" element={<Home/>} />
+            </Routes>
+            <Footer/>
+        </Router>
+       
+        </>
+       
+    );
+}
+
+export default App;
---
+++ myapp/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>MARCOS</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+         
+          <a href="https://instagram.com/i_saurabh_singh" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/one-saurabhsingh" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
